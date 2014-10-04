a0441a861f1de4b82a4883afa214072aee768f64
PJ Norberg <email@johannorberg>
2014-10-04 17:02:02 +0200

    skeleton

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* Plugin Name: WP Batch import postmeta
+* Plugin URI: https://github.com/pjnorberg/wp-batch-import-postmeta
+* Description: Export and import your Wordpress posts of any type to a handy CSV file, with your own post meta fields definitions.
+* Version: 1.0
+* Author: Johan Norberg (@pjnorberg)
+* Author URI: http://johannorberg.biz
+*/
+
+namespace pjnorberg;
+
+class BatchConvert {
+
+    public function __construct() {
+
+    }
+}
+
+$BatchConvert = new BatchConvert();
